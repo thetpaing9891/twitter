ffede3ab99e57e40cb3e0d2d2790db1693df7be8
Thet Paing Soe <thetpaing.soe@kbzbank.com>
2022-06-23 16:35:42 +0630

    Create Feeds Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Feeds from '../components/Feeds'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <Sidebar />
 
         {/* Feeds */}
+        <Feeds />
 
         {/* Widgets */}
 
